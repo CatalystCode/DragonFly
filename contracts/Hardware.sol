82535271d8f953b96b49276953eb3aae333497ec
Jason Poon <git@jasonpoon.ca>
2017-08-23 15:04:58 -0400

    new device api

--- contracts/Hardware.sol
+++ contracts/Hardware.sol
@@ -1,39 +1,53 @@
 pragma solidity ^0.4.2;
 
 contract Hardware {
+    address _owner;
+
     string public serial;
     string public assetTag;
     uint public ramSize;
     uint public hddSize;
     string public userid;
 
-    function Hardware (string _serial, string _assetTag, uint _ramSize, uint _hddSize) {
+    function Hardware(string _serial, string _assetTag, uint _ramSize, uint _hddSize) {
+        _owner = msg.sender;
+
         serial = _serial;
         assetTag = _assetTag;
         ramSize = _ramSize;
         hddSize = _hddSize;
-
     }
 
-//    function addLaptop () {
-//    }
-
     function updateHardware (uint newRamSize, uint newHddSize) {
+        if (msg.sender != _owner) {
+            revert();
+        }
+
         ramSize = newRamSize;
         hddSize = newHddSize;
     }
 
     function assignNewAssetTag (string newAssetTag) {
+        if (msg.sender != _owner) {
+            revert();
+        }
+
         assetTag = newAssetTag;
     }
 
     function assignToUser (string _userid) {
+        if (msg.sender != _owner) {
+            revert();
+        }
+
         userid = _userid;
-        
     }
 
     function freeLaptop () {
+        if (msg.sender != _owner) {
+            revert();
+        }
+        
         userid = "";
     }
-
 }
--- src/js/hardware.js
+++ src/js/hardware.js
@@ -1,9 +1,51 @@
 var Hardware = {
     web3Provider: null,
-    contracts: {},
+    contract: null, 
   
     init: function() {
-        console.log('test');
+        // Initialize web3 and set the provider to the testRPC.
+        if (typeof web3 !== 'undefined') {
+            Hardware.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // set the provider you want from Web3.providers
+            Hardware.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+            web3 = new Web3(Hardware.web3Provider);
+        }
+
+        web3.eth.defaultAccount = web3.eth.accounts[0];
+
+        Hardware.initContract();
     },
-  
-  };
+ 
+    initContract: function() {
+        $.getJSON('Hardware.json', function(data) {
+            // Get the necessary contract artifact file and instantiate it with truffle-contract.
+            var HardwareArtifact = data;
+            Hardware.contract = TruffleContract(HardwareArtifact);
+          
+            // Set the provider for our contract.
+            Hardware.contract.setProvider(Hardware.web3Provider);
+        });
+    },
+
+    newDevice: function(serial, assetTag, ramSize, hddSize) {
+        return new Promise((resolve, reject) => {
+            Hardware.contract.new(serial, assetTag, ramSize, hddSize).then(contract => {
+                if (!contract.address) {
+                    console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
+                } else {
+                    console.log("Contract mined! Address: " + contract.address);
+                }
+                resolve(contract);
+            })
+        });
+    }
+};
+
+// such a hack
+$(function() {
+    $(window).load(function() {
+      Hardware.init();
+    });
+  });
