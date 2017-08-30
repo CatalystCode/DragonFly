7164ef432f75f51efd2a262255ac51539ec735db
ali92hm <ali92hm@gmail.com>
2017-08-30 10:53:58 -0700

    clean ups

--- src/js/hardware.js
+++ src/js/hardware.js
@@ -1,106 +1,106 @@
 var Hardware = {
-    web3Provider: null,
+  web3Provider: null,
 
-    init: function() {
+  init: function () {
         // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-        if (typeof web3 !== 'undefined') {
+    if (typeof web3 !== 'undefined') {
             // Use Mist/MetaMask's provider
-            Hardware.web3Provider = web3.currentProvider;
-            web3 = new Web3(web3.currentProvider);
-        } else {
+      Hardware.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
             // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-            alert('No web3? You should consider using MetaMask')
-            Hardware.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-            web3 = new Web3(Hardware.web3Provider);
-        }
+      alert('No web3? You should consider using MetaMask')
+      Hardware.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+      web3 = new Web3(Hardware.web3Provider)
+    }
 
-        web3.eth.defaultAccount = web3.eth.accounts[0];
-    },
+    web3.eth.defaultAccount = web3.eth.accounts[0]
+  },
 
-    getContract: function() {
-        return new Promise((resolve, reject) => {
-            $.getJSON('Hardware.json', function(data) {
+  getContract: function () {
+    return new Promise((resolve, reject) => {
+      $.getJSON('Hardware.json', function (data) {
                 // Get the necessary contract artifact file and instantiate it with truffle-contract.
-                let contract = TruffleContract(data);
+        let contract = TruffleContract(data)
 
                 // Set the provider for our contract.
-                contract.setProvider(Hardware.web3Provider);
+        contract.setProvider(Hardware.web3Provider)
 
-                resolve(contract);
-            });
-        });
-    },
+        resolve(contract)
+      })
+    })
+  },
 
-    newDevice: function(serial, assetTag, ramSize, hddSize) {
-        return new Promise((resolve, reject) => {
-            Hardware.contract.new(serial, assetTag, ramSize, hddSize).then(contract => {
-                if (!contract.address) {
-                    console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
-                } else {
-                    console.log("Contract mined! Address: " + contract.address);
-                }
-                resolve(contract);
-            })
-        });
-    },
-
-    assignToUser: function(contractAddress, userId) {
-        let contract = Hardware.contract.at(contractAddress)
-        if (contract != null) {
-            contract.assignToUser(userId);
+  newDevice: function (serial, assetTag, ramSize, hddSize) {
+    return new Promise((resolve, reject) => {
+      Hardware.contract.new(serial, assetTag, ramSize, hddSize).then(contract => {
+        if (!contract.address) {
+          console.log('Contract transaction send: TransactionHash: ' + contract.transactionHash + ' waiting to be mined...')
+        } else {
+          console.log('Contract mined! Address: ' + contract.address)
         }
-    },
+        resolve(contract)
+      })
+    })
+  },
 
-    newAssetTag: function(contractAddress, assetTag) {
-        let contract = Hardware.contract.at(contractAddress)
-        if (contract != null) {
-            contract.assignNewAssetTag(assetTag);
-        }
-    },
+  assignToUser: function (contractAddress, userId) {
+    let contract = Hardware.contract.at(contractAddress)
+    if (contract != null) {
+      contract.assignToUser(userId)
+    }
+  },
 
-    updateHardware: function(contractAddress, newRamSize, newHDDSize) {
-        let contract = Hardware.contract.at(contractAddress)
-        if (contract != null) {
-            return contract.updateHardware(newRamSize, newHDDSize);
-        }
-    },
+  newAssetTag: function (contractAddress, assetTag) {
+    let contract = Hardware.contract.at(contractAddress)
+    if (contract != null) {
+      contract.assignNewAssetTag(assetTag)
+    }
+  },
 
-    freeLaptop: function(contractAddress) {
-        let contract = Hardware.contract.at(contractAddress)
-        if (contract != null) {
-            return contract.freeLaptop();
-        }
-    },
+  updateHardware: function (contractAddress, newRamSize, newHDDSize) {
+    let contract = Hardware.contract.at(contractAddress)
+    if (contract != null) {
+      return contract.updateHardware(newRamSize, newHDDSize)
+    }
+  },
 
-    getDevice: function(contractAddress) {
-        return this.getContract(contractAddress)
+  freeLaptop: function (contractAddress) {
+    let contract = Hardware.contract.at(contractAddress)
+    if (contract != null) {
+      return contract.freeLaptop()
+    }
+  },
+
+  getDevice: function (contractAddress) {
+    return this.getContract(contractAddress)
             .then(contract => {
-                contract = contract.at(contractAddress);
+              contract = contract.at(contractAddress)
 
-                if (!contract) {
-                    return;
-                }
+              if (!contract) {
+                return
+              }
 
-                var p1 = contract.serial.call();
-                var p2 = contract.assetTag.call();
-                var p3 = contract.ramSize.call();
-                var p4 = contract.hddSize.call();
-                var p5 = contract.userid.call();
+              var p1 = contract.serial.call()
+              var p2 = contract.assetTag.call()
+              var p3 = contract.ramSize.call()
+              var p4 = contract.hddSize.call()
+              var p5 = contract.userid.call()
 
-                return Promise.all([p1, p2, p3, p4, p5]).then(values => {
+              return Promise.all([p1, p2, p3, p4, p5]).then(values => {
                     // oh my god. I'm so sorry.
-                    values[2] = values[2].toNumber();
-                    values[3] = values[3].toNumber();
+                values[2] = values[2].toNumber()
+                values[3] = values[3].toNumber()
 
                     // [ "serial", "assetTag", 0, 0, "userId"]
-                    return values;
-                });
+                return values
+              })
             })
-    }
-};
+  }
+}
 
-$(function() {
-    $(window).load(function() {
-      Hardware.init();
-    });
-});
+// $(function() {
+//     $(window).load(function() {
+//       Hardware.init();
+//     });
+// });
